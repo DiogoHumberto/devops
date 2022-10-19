b594214774b76ff7ce8268b2a3c180d820e95152
BRQ\dbosco <dbosco@brq.com>
2022-10-19 14:36:57 -0300

    adicionando projeto spring e jenkinsfile

---
+++ devops-eventos/src/main/java/com/devopseventos/DevopsEventosApplication.java
@@ -0,0 +1,13 @@
+package com.devopseventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsEventosApplication.class, args);
+	}
+
+}
---
+++ devops-eventos/src/test/java/com/devopseventos/DevopsEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devopseventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
